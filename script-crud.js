fc26b9756facdc0bf197dd5068dc7479c43605a1
Leticia1508 <leticiademoraes0@gmail.com>
2024-01-22 21:18:43 -0300

    feat:Adicionando uma lista de tarefas

--- script-crud.js
+++ script-crud.js
@@ -4,13 +4,16 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 
-btnAdicionarTarefa.addEventListener("click", () => {
-  formAdicionarTarefa.classList.toggle("hidden");
-});
+const tarefas = []
 
-formAdicionarTarefa.addEventListener("submit", (evento) => {
-  evento.preventDefaut();
-  const tarefa = {
-    descricao: textarea.Value
-  }
+btnAdicionarTarefa.addEventListener('click', () => {
+  formAdicionarTarefa.classList.toggle('hidden');
 });
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+ const tarefa = {
+    descricao:textarea.value
+  }
+  tarefas.push(tarefa)
+  localStorage.setItem('tarefas', JSON.stringify(tarefas))
+})
